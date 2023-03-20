667f931c5feb3ff83adbf7e88d7b2820e70ea78c
Jangwook Ryu <rxjw95@gmail.com>
2023-03-21 03:55:03 +0900

    feature(jpa adapter 기능 추가)
    - loadOne
    - save

--- blog-adapter/src/main/java/com/assign/search/infrastructure/persistence/KeywordJpaAdapter.java
+++ blog-adapter/src/main/java/com/assign/search/infrastructure/persistence/KeywordJpaAdapter.java
@@ -1,12 +1,15 @@
 package com.assign.search.infrastructure.persistence;
 
 import com.assign.search.application.out.persistence.LoadHotKeywordPort;
+import com.assign.search.application.out.persistence.LoadKeywordPort;
+import com.assign.search.application.out.persistence.SaveKeywordPort;
 import com.assign.search.domain.Keyword;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Component;
 
 @Component
-public class KeywordJpaAdapter implements LoadHotKeywordPort {
+public class KeywordJpaAdapter implements LoadKeywordPort, LoadHotKeywordPort, SaveKeywordPort {
 
     private final KeywordRepository keywordRepository;
 
@@ -15,8 +18,17 @@ public class KeywordJpaAdapter implements LoadHotKeywordPort {
     }
 
     @Override
+    public Optional<Keyword> loadOne(String keyword) {
+        return keywordRepository.findById(keyword);
+    }
+
+    @Override
     public List<Keyword> loadKeywordTopTen() {
         return keywordRepository.findTopTenKeyword();
     }
 
+    @Override
+    public void save(Keyword keyword) {
+        keywordRepository.save(keyword);
+    }
 }
