b732b2874b89b5ea34c55f1cd1230fec93bdc65a
Jangwook Ryu <rxjw95@gmail.com>
2023-03-20 23:02:11 +0900

    any(패키지 변경과 컨벤션 변경 수정으로 인한 변경)

--- blog-adapter/src/test/java/com/assign/search/persentation/BlogSearchControllerTest.java
+++ blog-adapter/src/test/java/com/assign/search/persentation/BlogSearchControllerTest.java
@@ -10,10 +10,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.assign.search.application.in.usecase.BlogSearchUseCase;
+import com.assign.search.dto.BlogDocument;
+import com.assign.search.dto.PageInfo;
 import com.assign.search.dto.request.KeywordSearchRequest;
 import com.assign.search.dto.response.KeywordSearchResponse;
-import com.assign.search.vo.BlogDocument;
-import com.assign.search.vo.PageInfo;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -32,65 +32,65 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 @WebMvcTest
 class BlogSearchControllerTest {
 
-  @Autowired
-  private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-  @RegisterExtension
-  final RestDocumentationExtension restDocumentation = new RestDocumentationExtension(
-      "build/generated-snippets");
+    @RegisterExtension
+    final RestDocumentationExtension restDocumentation = new RestDocumentationExtension(
+        "build/generated-snippets");
 
-  @MockBean
-  private BlogSearchUseCase blogSearchUseCase;
+    @MockBean
+    private BlogSearchUseCase blogSearchUseCase;
 
-  @BeforeEach
-  void setUp(WebApplicationContext webApplicationContext,
-      RestDocumentationContextProvider restDocumentation) {
-    this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-        .apply(documentationConfiguration(restDocumentation))
-        .addFilters(new CharacterEncodingFilter("UTF-8"))
-        .build();
-  }
+    @BeforeEach
+    void setUp(WebApplicationContext webApplicationContext,
+        RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .apply(documentationConfiguration(restDocumentation))
+            .addFilters(new CharacterEncodingFilter("UTF-8"))
+            .build();
+    }
 
-  @Test
-  void search() throws Exception {
-    final String restKeyDummy = "KakaoAK 1029384756abc";
+    @Test
+    void search() throws Exception {
+        final String restKeyDummy = "KakaoAK 1029384756abc";
 
-    KeywordSearchRequest request = createKeywordSearchRequest();
+        KeywordSearchRequest request = createKeywordSearchRequest();
 
-    List<BlogDocument> blogDocuments = createDummyBlogDocuments();
-    PageInfo pageInfo = createDummyPageInfo();
-    KeywordSearchResponse response = new KeywordSearchResponse(blogDocuments, pageInfo);
+        List<BlogDocument> blogDocuments = createDummyBlogDocuments();
+        PageInfo pageInfo = createDummyPageInfo();
+        KeywordSearchResponse response = new KeywordSearchResponse(blogDocuments, pageInfo);
 
-    given(blogSearchUseCase.search(request)).willReturn(response);
+        given(blogSearchUseCase.search(request)).willReturn(response);
 
-    mockMvc.perform(get("/blog/search")
-            .accept(APPLICATION_JSON)
-            .header(AUTHORIZATION, restKeyDummy)
-            .param("query", "keyword")
-        )
-        .andExpect(status().isOk())
-        .andDo(print())
-        .andDo(searchKeywordRestDocument());
-  }
+        mockMvc.perform(get("/blog/search")
+                .accept(APPLICATION_JSON)
+                .header(AUTHORIZATION, restKeyDummy)
+                .param("query", "keyword")
+            )
+            .andExpect(status().isOk())
+            .andDo(print())
+            .andDo(searchKeywordRestDocument());
+    }
 
-  private KeywordSearchRequest createKeywordSearchRequest() {
-    return new KeywordSearchRequest("keyword", null, null, null);
-  }
+    private KeywordSearchRequest createKeywordSearchRequest() {
+        return new KeywordSearchRequest("keyword", null, null, null);
+    }
 
-  private List<BlogDocument> createDummyBlogDocuments() {
-    BlogDocument blogDocument = BlogDocument.of(
-        "블로그 명",
-        "블로그 컨텐츠",
-        LocalDateTime.of(2023, 3, 18, 10, 0),
-        "https://search3.kakaocdn.net",
-        "게시글 제목",
-        "https://developers.kakao.com/docs/latest/ko/daum-search/dev-guide#search-blog");
+    private List<BlogDocument> createDummyBlogDocuments() {
+        BlogDocument blogDocument = BlogDocument.of(
+            "블로그 명",
+            "블로그 컨텐츠",
+            LocalDateTime.of(2023, 3, 18, 10, 0),
+            "https://search3.kakaocdn.net",
+            "게시글 제목",
+            "https://developers.kakao.com/docs/latest/ko/daum-search/dev-guide#search-blog");
 
-    return List.of(blogDocument);
-  }
+        return List.of(blogDocument);
+    }
 
-  private PageInfo createDummyPageInfo() {
-    return new PageInfo(100, 50, false);
-  }
+    private PageInfo createDummyPageInfo() {
+        return new PageInfo(100, 50, false);
+    }
 
 }
--- blog-core/src/main/java/com/assign/search/dto/response/KeywordSearchResponse.java
+++ blog-core/src/main/java/com/assign/search/dto/response/KeywordSearchResponse.java
@@ -1,7 +1,7 @@
 package com.assign.search.dto.response;
 
-import com.assign.search.vo.BlogDocument;
-import com.assign.search.vo.PageInfo;
+import com.assign.search.dto.BlogDocument;
+import com.assign.search.dto.PageInfo;
 import java.util.List;
 
 public record KeywordSearchResponse(List<BlogDocument> documents, PageInfo pageInfo) {
