4eaa3d480cc60549586cf182a5316bb61021c09a
Jangwook Ryu <rxjw95@gmail.com>
2023-03-21 02:54:55 +0900

    feature(Keyword 도메인 생성)
    - keyword, frequecy

---
+++ blog-core/src/main/java/com/assign/search/domain/Keyword.java
@@ -0,0 +1,37 @@
+package com.assign.search.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import org.springframework.data.domain.Persistable;
+
+@Entity
+@Getter
+public class Keyword implements Persistable<String> {
+
+    @Id
+    private String keyword;
+    private Integer searchFrequency;
+
+    public Keyword(String keyword, Integer searchFrequency) {
+        this.keyword = keyword;
+        this.searchFrequency = searchFrequency;
+    }
+
+    public void addFrequency(int frequency) {
+        searchFrequency += frequency;
+    }
+    
+    @Override
+    public String getId() {
+        return this.keyword;
+    }
+
+    @Override
+    public boolean isNew() {
+        return true;
+    }
+
+    protected Keyword() {
+    }
+}
