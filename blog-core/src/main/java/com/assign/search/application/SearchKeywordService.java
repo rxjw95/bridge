fcc6674504e841efd5bc32cd686a83f731eb6ada
Jangwook Ryu <rxjw95@gmail.com>
2023-03-21 03:27:06 +0900

    feature(키워드 검색 시 키워드 카운트 이벤트 추가)

--- blog-core/src/main/java/com/assign/search/application/SearchKeywordService.java
+++ blog-core/src/main/java/com/assign/search/application/SearchKeywordService.java
@@ -2,6 +2,7 @@ package com.assign.search.application;
 
 import com.assign.search.application.in.usecase.SearchKeywordUseCase;
 import com.assign.search.application.out.api.SearchClient;
+import com.assign.search.application.out.event.SearchEventPublisher;
 import com.assign.search.dto.request.KeywordSearchRequest;
 import com.assign.search.dto.response.KeywordSearchResponse;
 import org.springframework.stereotype.Service;
@@ -10,13 +11,16 @@ import org.springframework.stereotype.Service;
 public class SearchKeywordService implements SearchKeywordUseCase {
 
     private final SearchClient searchClient;
+    private final SearchEventPublisher publisher;
 
-    public SearchKeywordService(SearchClient searchClient) {
+    public SearchKeywordService(SearchClient searchClient, SearchEventPublisher publisher) {
         this.searchClient = searchClient;
+        this.publisher = publisher;
     }
 
     @Override
     public KeywordSearchResponse search(KeywordSearchRequest request) {
+        publisher.publish(request.getQuery());
         return searchClient.fetch(request);
     }
 
