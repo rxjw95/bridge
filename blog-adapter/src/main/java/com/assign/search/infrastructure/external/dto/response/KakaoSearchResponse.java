064542ae04ecb8d26d2153787e3ab1a830286e1d
Jangwook Ryu <rxjw95@gmail.com>
2023-03-21 23:49:35 +0900

    refactor(사용하지 않는 생성자 제거)
    - KakaoSearchResponse deserialize만 사용하기 때문에 기본 생성자만 추가

--- blog-adapter/src/main/java/com/assign/search/infrastructure/external/dto/response/KakaoSearchResponse.java
+++ blog-adapter/src/main/java/com/assign/search/infrastructure/external/dto/response/KakaoSearchResponse.java
@@ -2,19 +2,14 @@ package com.assign.search.infrastructure.external.dto.response;
 
 import java.util.List;
 import java.util.Map;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class KakaoSearchResponse {
 
     private List<Map<String, String>> documents;
     private Map<String, String> meta;
-
-    public KakaoSearchResponse(List<Map<String, String>> documents, Map<String, String> meta) {
-        this.documents = documents;
-        this.meta = meta;
-    }
-
-    protected KakaoSearchResponse() {
-    }
 }
