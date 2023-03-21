faf3a0d5a96353da7877767ac57c003273762ce4
Jangwook Ryu <rxjw95@gmail.com>
2023-03-22 05:50:02 +0900

    refactor(컨벤션 자동 변경)

--- blog-core/src/main/java/com/assign/search/dto/request/KeywordSearchRequest.java
+++ blog-core/src/main/java/com/assign/search/dto/request/KeywordSearchRequest.java
@@ -11,22 +11,23 @@ import org.hibernate.validator.constraints.Range;
 @EqualsAndHashCode
 public class KeywordSearchRequest {
 
-  @NotBlank
-  private final String query;
+    @NotBlank(message = "질의어는 필수값입니다.")
+    private final String query;
 
-  @Range(min = 1, max = 50, message = "1~50 사이의 값을 입력해주세요. [기본 값: 1]")
-  private final Integer page;
+    @Range(min = 1, max = 50, message = "1~50 사이의 값을 입력해주세요. [기본 값: 1]")
+    private final Integer page;
 
-  @Range(min = 1, max = 50, message = "1~50 사이의 값을 입력해주세요. [기본 값: 10]")
-  private final Integer size;
+    @Range(min = 1, max = 50, message = "1~50 사이의 값을 입력해주세요. [기본 값: 10]")
+    private final Integer size;
 
-  @Pattern(regexp = "accuracy|recency", message = "accuracy(정확도순) 또는 recency(최신순)으로 정렬할 수 있습니다. [기본 값: accuracy]")
-  private final String sort;
+    @Pattern(regexp = "accuracy|recency", message = "accuracy(정확도순) 또는 recency(최신순)으로 정렬할 수 있습니다. [기본 값: accuracy]")
+    private final String sort;
 
-  public KeywordSearchRequest(String query, Integer page, Integer size, String sort) {
-    this.query = query;
-    this.page = Objects.requireNonNullElse(page, 1);
-    this.size = Objects.requireNonNullElse(size, 10);
-    this.sort = Objects.requireNonNullElse(sort, "accuracy");
-  }
+    public KeywordSearchRequest(String query, Integer page, Integer size, String sort) {
+        this.query = query;
+        this.page = Objects.requireNonNullElse(page, 1);
+        this.size = Objects.requireNonNullElse(size, 10);
+        this.sort = Objects.requireNonNullElse(sort, "accuracy");
+    }
+
 }
