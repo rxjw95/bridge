09301287a786c7d8f2f93d747e46e8bdba9d51bb
Jangwook Ryu <rxjw95@gmail.com>
2023-03-22 05:53:43 +0900

    refactor(PageInfo record 변경)

--- blog-core/src/main/java/com/assign/search/dto/PageInfo.java
+++ blog-core/src/main/java/com/assign/search/dto/PageInfo.java
@@ -1,20 +1,5 @@
 package com.assign.search.dto;
 
-import lombok.Getter;
+public record PageInfo(Integer totalCount, Integer pageableCount, boolean isLastPage) {
 
-@Getter
-public class PageInfo {
-
-    private int totalCount;
-    private int pageableCount;
-    private boolean isLastPage;
-
-    public PageInfo(int totalCount, int pageableCount, boolean isLastPage) {
-        this.totalCount = totalCount;
-        this.pageableCount = pageableCount;
-        this.isLastPage = isLastPage;
-    }
-
-    protected PageInfo() {
-    }
 }
